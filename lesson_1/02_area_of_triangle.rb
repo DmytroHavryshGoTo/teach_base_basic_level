465a39b762a747273c9b2642061642d0c8b31e98
Dima <formyblogger69@gmail.com>
2020-03-13 12:04:51 +0200

    TeachBase Lesson01 Task02 Area of triangle

---
+++ lesson_1/02_area_of_triangle.rb
@@ -0,0 +1,8 @@
+#Программа должна запрашивать основание и высоту треуголиника и возвращать его площадь
+
+puts "Введите длину основания треугольника"
+triangle_base = gets.chomp.to_f
+puts "Введите высоту треугольника"
+triangle_height = gets.chomp.to_f
+area_of_triangele = 1 / 2.0 * triangle_base * triangle_height
+puts "Площадь треугольника = #{area_of_triangele}"

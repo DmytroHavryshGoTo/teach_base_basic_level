3e7ea83d9f56ebc5818adac8a9eccca3269369a3
Dima <formyblogger69@gmail.com>
2020-03-15 17:00:53 +0200

    Teach_base Lesson02 Task04 Fixing bug with vowels

--- lesson_2/fill_hash_with_letters.rb
+++ lesson_2/fill_hash_with_letters.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
-# Fill the hash with letters where key is letter and value is position
+# Fill the hash with vowel letters where key is letter and value is position
 
 abc = {}
+vowels = %w[a e i o u]
 letters = ('a'..'z').to_a
 index = 0
 while index < letters.length
-  abc[letters[index]] = index + 1
+  abc[letters[index]] = (index + 1) if vowels.include? letters[index]
   index += 1
 end

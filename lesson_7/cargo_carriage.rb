6b2318a3718ad812e2962c9f2deceb40b3b6378d
Dima <formyblogger69@gmail.com>
2020-03-20 12:39:10 +0200

    TeachBase Lesson07 Task02 Adding total volume for cargo carriage

--- lesson_7/cargo_carriage.rb
+++ lesson_7/cargo_carriage.rb
@@ -4,6 +4,10 @@ require_relative 'carriage'
 
 # Derived Carriage class
 class CargoCarriage < Carriage
+  alias free_volume free_places
+  alias taken_volume taken_places
+  alias increase_taken_volume take_place
+
   def type
     :cargo
   end

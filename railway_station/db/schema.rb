2b092b7dfa1ddebbe4b1a19f35e682f7b30a848e
Dima <formyblogger69@gmail.com>
2020-03-25 21:00:00 +0200

    TeachBase Lesson11 Task01 Creating CRUD for Routes with validation

--- railway_station/db/schema.rb
+++ railway_station/db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_23_181503) do
+ActiveRecord::Schema.define(version: 2020_03_25_152307) do
+
+  create_table "routes", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "train_stations", force: :cascade do |t|
     t.string "title"
